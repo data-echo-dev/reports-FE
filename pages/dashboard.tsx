f8b7a031d5f9979381ddaba1718504bea028173d
Guide Fari <guideg6@gmail.com>
2021-08-09 23:43:02 +0200

    I keep forgetting to commit files ffs

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,11 +1,15 @@
 // @ts-nocheck
 import { useRequireAuth } from '../hooks/useRequireAuth'
+import LinksGrid from '../components/Grids/LinksGrid'
 import { db } from '../config/firebase'
+import { useState } from 'react'
 
 const DashBoardPage: React.FC = () => {
   const auth = useRequireAuth()
+  const [reports, setReports] = useState(null)
 
   if (!auth.user) return null
+  console.log(auth.user)
 
   async function getTeacherReports(auth): void {
     const reportsRef = db.collection('reports')
@@ -13,6 +17,7 @@ const DashBoardPage: React.FC = () => {
       .where('organisation', '==', auth.user.organisation)
       .where('teacher', '==', auth.user.uid)
       .get()
+    setReports(reportsTeacher)
     return reportsTeacher.forEach((doc) => {
       console.log(doc.id, '=>', doc.data())
     })
@@ -25,14 +30,92 @@ const DashBoardPage: React.FC = () => {
       .where('roles', 'array-contains-any', auth.user.roles)
       .get()
 
+    setReports(reportsOrg)
     return reportsOrg.forEach((doc) => {
+      console.log(doc.id, '=>', doc.data())
+    })
+  }
+  async function getOrgs(): void {
+    const orgsRef = db.collection('organisations')
+    const orgs = await orgsRef.get()
+
+    return orgs.forEach((doc) => {
+      console.log(doc.id, '=>', doc.data())
+    })
+  }
+
+  async function getUsers(): void {
+    const usersRef = db.collection('users')
+    const users = await usersRef.get()
+
+    return users.forEach((doc) => {
       console.log(doc.id, '=>', doc.data())
     })
   }
 
   return (
     <div className="flex min-h-screen bg-gray-200">
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="relative w-screen bg-white dark:bg-gray-800">
+        <div className="flex flex-col sm:flex-row sm:justify-around">
+          <div className="h-screen w-80">
+            <nav className="px-6 mt-10 ">
+              <button
+                className="flex items-start w-full p-2 my-6 text-left text-gray-600 transition-colors duration-200 rounded-lg hover:text-gray-800 hover:bg-gray-100 dark:hover:text-white dark:hover:bg-gray-600 dark:text-gray-400 "
+                onClick={() => getOrgReports(auth)}
+                disabled={!auth}
+              >
+                <span className="mx-4 text-lg font-normal">
+                  Organisation Reports
+                </span>
+                <span className="flex-grow text-right"></span>
+              </button>
+              <button
+                className="flex items-center w-full p-2 my-6 text-gray-800 transition-colors duration-200 rounded-lg hover:text-gray-800 hover:bg-gray-100 dark:hover:text-white dark:hover:bg-gray-600 dark:text-gray-100 dark:bg-gray-600"
+                onClick={() => getTeacherReports(auth)}
+                disabled={!auth}
+              >
+                <span className="mx-4 text-lg font-normal">My Reports</span>
+                <span className="flex-grow text-right"></span>
+              </button>
+              {auth.user.isSuperAdmin && (
+                <>
+                  <button
+                    className="flex items-center p-2 my-6 text-gray-600 transition-colors duration-200 rounded-lg hover:text-gray-800 hover:bg-gray-100 dark:hover:text-white dark:hover:bg-gray-600 dark:text-gray-400 "
+                    onClick={() => getOrgs()}
+                  >
+                    <span className="mx-4 text-lg font-normal">
+                      Org Management
+                    </span>
+                    <span className="flex-grow text-right"></span>
+                  </button>
+                  <button
+                    className="flex items-center p-2 my-6 text-gray-600 transition-colors duration-200 rounded-lg hover:text-gray-800 hover:bg-gray-100 dark:hover:text-white dark:hover:bg-gray-600 dark:text-gray-400 "
+                    onClick={() => getUsers()}
+                  >
+                    <span className="mx-4 text-lg font-normal">
+                      User Management
+                    </span>
+                    <span className="flex-grow text-right"></span>
+                  </button>
+                  <a
+                    className="flex items-center p-2 my-6 text-gray-600 transition-colors duration-200 rounded-lg hover:text-gray-800 hover:bg-gray-100 dark:hover:text-white dark:hover:bg-gray-600 dark:text-gray-400 "
+                    href="#"
+                  >
+                    <span className="mx-4 text-lg font-normal">
+                      Report Management
+                    </span>
+                    <span className="flex-grow text-right"></span>
+                  </a>
+                </>
+              )}
+            </nav>
+          </div>
+
+          <LinksGrid reportsData={reports} />
+        </div>
+      </div>
+
+      {/* <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="mt-24 text-center">
           <h2 className="mt-6 text-3xl font-extrabold text-center text-gray-900">
             {`Welcome ${auth.user.name}!`}
@@ -47,6 +130,7 @@ const DashBoardPage: React.FC = () => {
             <button onClick={() => getTeacherReports(auth)} disabled={!auth}>
               My Links
             </button>
+            {auth.user.isSuperAdmin && <button>Things</button>}
           </div>
           <button
             onClick={() => auth.signOut()}
@@ -55,7 +139,7 @@ const DashBoardPage: React.FC = () => {
             Sign out
           </button>
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
