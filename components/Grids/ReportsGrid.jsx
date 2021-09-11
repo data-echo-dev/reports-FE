42aabe9ce86f82439e9d783848a20ec0bf83aeb9
Guide Fari <guideg6@gmail.com>
2021-09-11 08:34:43 +0200

    feat: grids work

--- components/Grids/ReportsGrid.jsx
+++ components/Grids/ReportsGrid.jsx
@@ -1,81 +1,81 @@
 import Link from 'next/link'
 import React from 'react'
+import {
+  Button,
+  Badge,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+} from '@chakra-ui/react'
 
-const ReportsGrid = ({reportsData}) => {
-    let cleanedUp = []
-    const thingy = []
-    reportsData?.forEach((doc) => {
-        thingy.push(doc.id)
-        cleanedUp.push(doc.data())
-      })
-      console.log(thingy);
-    
-    return (
-        <div className="flex items-center justify-center w-full min-h-screen bg-gray-900">
-    <div className="col-span-12">
-      <div className="overflow-auto lg:overflow-visible ">
-          {cleanedUp.length > 0 && (
-            <table className="table space-y-6 text-sm text-gray-400 border-separate">
-            <thead className="text-gray-500 bg-gray-800">
-                <tr>
-                <th className="p-3 text-left">Title</th>
-                {/* <th className="p-3 text-left">URL</th> */}
-                <th className="p-3 text-left">Organisation</th>
-                <th className="p-3 text-left">Roles</th>
-                <th className="p-3 text-left">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    cleanedUp.map((report) => (
-                    <tr key={report.id} className="bg-gray-800">
-                    <td className="p-3">
-                        <div className="flex align-items-center">
-                        <div >{report.title}</div>
-                        </div>
-                    </td>
-                    {/* <td className="p-3">
+const ReportsGrid = ({ reportsData }) => (
+  <div className="flex items-center justify-center w-full h-full ">
+    {reportsData.length > 0 && (
+      <Table className="max-w-6xl" colorScheme="facebook" variant="striped">
+        <TableCaption>My Reports</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>Title</Th>
+            {/* <Th >URL</Th> */}
+            <Th>Organisation</Th>
+            <Th>Roles</Th>
+            <Th>Actions</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {reportsData.map((report) => (
+            <Tr key={report.id}>
+              <Td>
+                <div>
+                  <div>{report.title}</div>
+                </div>
+              </Td>
+              {/* <Td >
 
                         {report.url}
-                    </td> */}
-                    <td className="p-3">
-                        {report.organisation}
-                    </td>
-                    <td className="p-3">
-                        {report.roles?.map((role) => <span key={`${role}-your-boat`}>{role}</span>)}
-                    </td>
-                    <td className="p-3 whitespace-nowrap">
-                        <span className='flex items-center justify-center w-full'>
-                            <Link 
-                                href={{
-                                pathname: '/report',
-                                query: { id: `${report.id}` },
-                                }} 
-                                target='_blank' 
-                                className="text-gray-400 hover:text-gray-100"
-                                >
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6 mx-auto" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
-                                    </svg>                    
-                            </Link>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                            </svg>
-                            <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                            </svg>
-                        </span>
-                    </td>
-                    </tr>
-                    ))
-                }
-            </tbody>
-            </table>
-          )}
-      </div>
-    </div>
+                    </Td> */}
+              <Td>{report.organisation}</Td>
+              <Td>
+                {report.roles?.map((role) => (
+                  <span key={`${role}-your-boat`}>{role}</span>
+                ))}
+              </Td>
+              <Td>
+                <span className="flex items-center justify-center w-full">
+                  <Link
+                    href={{
+                      pathname: '/report',
+                      query: { id: `${report.id}` },
+                    }}
+                    target="_blank"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="w-6 h-6 mx-auto text-gray-400 hover:text-gray-100 hover:cursor-pointer"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={2}
+                        d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                      />
+                    </svg>
+                  </Link>
+                </span>
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    )}
   </div>
-    )
-}
+)
 
 export default ReportsGrid
--- pages/my-reports.tsx
+++ pages/my-reports.tsx
@@ -1,50 +1,68 @@
 // @ts-nocheck
 import { useEffect, useRef, useState } from 'react'
 import { useRequireAuth } from '../hooks/useRequireAuth'
+import { useFirestoreQuery } from '../hooks/useFirestoreQuery'
+
 import ReportsGrid from '../components/Grids/ReportsGrid'
 import { db } from '../config/firebase'
+import PageTitle from '../components/PageTitle'
 
 const MyReports = () => {
-  // TODO: change the data fetching to useHook
+  const auth = useRequireAuth()
+  const { data, status, error } = useFirestoreQuery(
+    db
+      .collection('reports')
+      // .where('organisation', '==', auth.user.organisation)
+      .where('teacher', '==', auth.user?.uid)
+  )
+  if (status === 'loading') {
+    return 'Loading...'
+  }
+  if (status === 'error') {
+    return `Error: ${error.message}`
+  }
 
-  useEffect(() => {
-    isMounted.current = true
-    fetchData()
-    // this is run when component unmount
-    return () => (isMounted.current = false)
-  }, [])
+  // useEffect(() => {
+  //   isMounted.current = true
+  //   fetchData()
+  //   // this is run when component unmount
+  //   return () => (isMounted.current = false)
+  // }, [])
 
   // Create Ref
-  const isMounted = useRef(false)
+  // const isMounted = useRef(false)
   // Create Your Required States
-  const [reports, setReports] = useState(null)
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState('')
-  const auth = useRequireAuth()
+  // const [reports, setReports] = useState(null)
+  // const [isLoading, setIsLoading] = useState(false)
+  // const [error, setError] = useState('')
 
   if (!auth.user) return null
   // Create a function for fetching your data
-  const fetchData = () => {
-    const reportsRef = db.collection('reports')
-    reportsRef
-      .where('organisation', '==', auth.user.organisation)
-      .where('teacher', '==', auth.user.uid)
-      .get()
-      .then((response) => {
-        if (isMounted.current) {
-          setReports(response)
-          setIsLoading(false)
-        }
-      })
-      .catch((error) => {
-        // check ref before updating state
-        isMounted.current && setError(error)
-      })
-  }
+  // const fetchData = () => {
+  //   const reportsRef = db.collection('reports')
+  //   reportsRef
+  //     .where('organisation', '==', auth.user.organisation)
+  //     .where('teacher', '==', auth.user.uid)
+  //     .get()
+  //     .then((response) => {
+  //       if (isMounted.current) {
+  //         setReports(response)
+  //         setIsLoading(false)
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       // check ref before updating state
+  //       isMounted.current && setError(error)
+  //     })
+  // }
 
   return (
-    <div>
-      <ReportsGrid reportsData={reports} />
+    <div className="w-full">
+      <PageTitle text="My Reports" />
+
+      <div className="flex flex-col justify-center">
+        <ReportsGrid reportsData={data} />
+      </div>
     </div>
   )
 }
