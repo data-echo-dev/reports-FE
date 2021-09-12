5a4a06dc4590e0b32daba97c126b2b197e9a72e0
Guide Fari <guideg6@gmail.com>
2021-09-12 12:52:24 +0200

    core: remove SVG icon & replace with heroicon

--- components/Grids/OrgsGrid.jsx
+++ components/Grids/OrgsGrid.jsx
@@ -13,6 +13,7 @@ import {
 } from '@chakra-ui/react'
 
 import DeleteOrgButton from '../Buttons/DeleteOrgButton'
+import { PencilIcon } from '@heroicons/react/outline'
 
 const OrgsGrid = ({ orgsData }) => (
   <div className="flex items-center justify-center w-full h-full ">
@@ -46,20 +47,7 @@ const OrgsGrid = ({ orgsData }) => (
                 <span className="flex items-center justify-start w-full space-x-2">
                   <Link href={`/organisation/${org.id}`}>
                     <Button size="sm" colorScheme="facebook">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="w-5 h-5 text-blue-300"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                        />
-                      </svg>
+                      <PencilIcon className='w-5 h-5 text-blue-300'/>
                     </Button>
                   </Link>
                   <DeleteOrgButton orgID={org.id} />
