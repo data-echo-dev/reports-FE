d1e75368d93628c40b3a13d08802c03318721572
Guide Fari <guideg6@gmail.com>
2021-09-25 17:26:36 +0200

    feat: skeleton of single user page

--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import { db } from '../../config/firebase'
+import { useFirestoreQuery } from '../../hooks/useFirestoreQuery'
+import { useRequireAuth } from '../../hooks/useRequireAuth'
 
-const SingleUser = ({ params: { id } }) => (
-  <>
-    <h1>user</h1>
-    <div>{id}</div>
-  </>
-)
+const SingleUser = ({ params: { id } }) => {
+  const auth = useRequireAuth()
+
+  // state
+
+  // Subscribe to Firestore document
+  const { data, status, error } = useFirestoreQuery(
+    db.collection('users').doc(id)
+  )
+  console.log(data)
+
+  return (
+    <>
+      <h1>user</h1>
+      <div>{id}</div>
+    </>
+  )
+}
 
 export default SingleUser
 
