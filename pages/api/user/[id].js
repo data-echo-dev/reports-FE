d8e066977bf366ab0a8733669a3d197413c199b0
Guide Fari <guideg6@gmail.com>
2021-09-30 23:46:25 +0200

    docs: update todo

--- pages/api/user/[id].js
+++ pages/api/user/[id].js
@@ -1,12 +1,10 @@
 import { auth, db } from '../../../config/firebase-admin'
 
-// TODO: write code to delete user
-// TODO: write code to update user details
-// update & delete have to take care of firestore(email address) & authentication (identifier)
+// TODO: figure out how to properly resolve these requests for the frontend
 
 export default (req, res) => {
-    const uid = req.query.id
-    
+  const uid = req.query.id
+
   switch (req.method) {
     case 'GET':
       db.collection('users')
@@ -21,15 +19,20 @@ export default (req, res) => {
       // ...
       break
     case 'POST':
-      db.collection('users').doc(uid).update(req.body)
-      .then(() => {auth.updateUser(uid, req.body)})
-      .then((userRecord) => res.json(userRecord))
-      .catch(error => res.json({error}))
+      db.collection('users')
+        .doc(uid)
+        .update(req.body)
+        .then(() => {
+          auth.updateUser(uid, req.body)
+        })
+        .then((userRecord) => res.json(userRecord))
+        .catch((error) => res.json({ error }))
       break
     case 'DELETE':
-      auth.deleteUser(uid)
-      .then(() => console.log('jahman vabaya'))
-      .catch(error => res.json({error}))
+      auth
+        .deleteUser(uid)
+        .then(() => console.log('jahman vabaya'))
+        .catch((error) => res.json({ error }))
       break
     default:
       res.status(405).end() // Method Not Allowed
