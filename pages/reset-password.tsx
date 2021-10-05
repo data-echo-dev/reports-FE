cc1cdfde4408df1862d634dd7d8fc75bb577ba83
Guide Fari <guideg6@gmail.com>
2021-10-05 18:55:32 +0200

    styling: update background color on auth pages

--- pages/reset-password.tsx
+++ pages/reset-password.tsx
@@ -1,28 +1,26 @@
 import Link from 'next/link'
 import ResetPasswordForm from '../components/forms/ResetPasswordForm'
 
-const ResetPasswordPage: React.FC = () => {
-  return (
-    <div className="flex min-h-screen bg-gray-200">
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="mt-24 text-center">
-          <h2 className="mt-6 text-3xl font-extrabold text-center text-gray-900">
-            Reset password
-          </h2>
-          <p className="mt-2 text-center text-gray-600 text-md">
-            {"Didn't forgot? "}
-            <Link href="/login">
-              <a href="#" className="text-blue-500">
-                Login
-              </a>
-            </Link>
-          </p>
-        </div>
-        <div className="px-4 py-8 mt-8 bg-white shadow sm:rounded-lg sm:px-10">
-          <ResetPasswordForm />
-        </div>
+const ResetPasswordPage: React.FC = () => (
+  <div className="flex min-h-screen bg-primary-white">
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="mt-24 text-center">
+        <h2 className="mt-6 text-3xl font-extrabold text-center text-gray-900">
+          Reset password
+        </h2>
+        <p className="mt-2 text-center text-gray-600 text-md">
+          {"Didn't forgot? "}
+          <Link href="/login">
+            <a href="#" className="text-blue-500">
+              Login
+            </a>
+          </Link>
+        </p>
+      </div>
+      <div className="px-4 py-8 mt-8 bg-white shadow sm:rounded-lg sm:px-10">
+        <ResetPasswordForm />
       </div>
     </div>
-  )
-}
+  </div>
+)
 export default ResetPasswordPage
--- pages/signup.tsx
+++ pages/signup.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 import SignUpForm from '../components/forms/SignUpForm'
 
 const SignUpPage: React.FC = () => (
-  <div className="flex flex-col justify-center w-full min-h-screen bg-gray-200">
+  <div className="flex flex-col justify-center w-full min-h-screen bg-primary-white">
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div className="mt-24 text-center">
         <h2 className="mt-6 text-3xl leading-9 text-center text-gray-900 font- extrabold">
