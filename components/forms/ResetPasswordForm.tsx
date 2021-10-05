bf4d3c25fc7d59060cd447e96a237f14fbe43e6e
Guide Fari <guideg6@gmail.com>
2021-10-05 21:22:20 +0200

    styling: auth pages

--- components/forms/ResetPasswordForm.tsx
+++ components/forms/ResetPasswordForm.tsx
@@ -1,6 +1,7 @@
 // @ts-nocheck
 import Router from 'next/router'
 import { useState } from 'react'
+import { Button, Input } from '@chakra-ui/react'
 import { useAuth } from '../../hooks/useAuth'
 
 const ResetPasswordForm: React.FC = () => {
@@ -32,22 +33,24 @@ const ResetPasswordForm: React.FC = () => {
       >
         Email address
       </label>
-      <input
+      <Input
         id="email"
-        className="block w-full px-3 py-2 placeholder-gray-400 transition duration-150 ease-in-out border border-gray-300 rounded-md appearance-none focus:outline-none focus:shadow-outline-blue focus:border-blue-300 sm:text-sm sm:leading-5"
         type="email"
         name="email"
         onChange={(e) => setEmail(e.target.value)}
       />
       <div className="mt-6">
         <span className="block w-full rounded-md shadow-sm">
-          <button
+          <Button
             type="button"
-            className="flex justify-center w-full px-4 py-2 text-sm font-medium text-white transition duration-150 ease-in-out bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700"
+            bgColor="primary.blue"
+            className="opacity-80 hover:text-primary-blue"
+            isFullWidth
+            color="primary.white"
             onClick={() => onSubmit({ email })}
           >
             Send reset link
-          </button>
+          </Button>
         </span>
       </div>{' '}
     </form>
