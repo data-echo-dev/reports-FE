8dde753bd6e01a1ebe103420cf35d6b7dcbb932d
Guide Fari <guideg6@gmail.com>
2021-10-06 19:50:13 +0200

    feat: add nProgress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import Router from 'next/router'
+import NProgress from 'nprogress'
 import { AuthProvider } from '../hooks/useAuth'
+import 'nprogress/nprogress.css'
 import Layout from '../components/Layout'
 
 import '../styles/base.css'
 import { theme } from '../styles/chakraTheme'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
