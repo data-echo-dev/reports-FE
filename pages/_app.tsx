fa0e8166de80859d0060f43c61fe575b8ed3f1b2
Guide Fari <guideg6@gmail.com>
2021-10-13 19:45:23 +0200

    misc: add favicon & title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 // @ts-nocheck
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { ChakraProvider } from '@chakra-ui/react'
 import Router from 'next/router'
 import NProgress from 'nprogress'
@@ -19,6 +20,10 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     <AuthProvider>
       <ChakraProvider theme={theme}>
         <Layout>
+          <Head>
+            <link rel="shortcut icon" href="/favicon.svg" />
+            <title>DataEcho - Hear it speak</title>
+          </Head>
           <Component {...pageProps} />
         </Layout>
       </ChakraProvider>
