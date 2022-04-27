f4360833f649a05d501db26e1814c614079db9b9
Guide Fari <guideg6@gmail.com>
2022-04-27 14:39:43 +0200

    chore: add fixtures

---
+++ src/app/fixtures/reports.ts
@@ -0,0 +1,9 @@
+import { Report } from '../types'
+
+export const defaultReport: Report = {
+  // this is the unassigned org ID
+  organisation: 'XNcDtlEkoTFw3ybonFua',
+  teacher: '',
+  title: '',
+  url: '',
+}
