3a3d30971cf30ac8309e53f8cc0fabe59814c268
Guide Fari <guideg6@gmail.com>
2022-07-25 21:38:56 +0200

    here we go again

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,11 +2,16 @@ import { test, expect } from '@playwright/test'
 
 test('Can we still login', async ({ page }) => {
   // Go to http://localhost:3000/
-  await page.goto('http://localhost:3000/')
+  await page.goto(
+    process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000'
+  )
 
   // Click text=Sign In
   await page.locator('text=Sign In').click()
-  await expect(page).toHaveURL('http://localhost:3000/login')
+  await expect(page).toHaveURL(
+    `${process.env.PLAYWRIGHT_TEST_BASE_URL}/login` ||
+      'http://localhost:3000/login'
+  )
 
   // Fill input[name="email"]
   await page.locator('input[name="email"]').fill(process.env.E2E_USERNAME)
@@ -16,5 +21,7 @@ test('Can we still login', async ({ page }) => {
 
   // Click text=Login
   await page.locator('text=Login').click()
-  await expect(page).toHaveURL('http://localhost:3000/my-reports')
+  await expect(page).toHaveURL(
+    `${process.env.PLAYWRIGHT_TEST_BASE_URL}/my-reports`
+  )
 })
